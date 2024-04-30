9fe4c3feb28ab5ca23dd5f1c557fda2725ff0366
omarbramirez <omar.ramirez94@hotmail.es>
2024-04-30 09:41:23 -0600

    favicon fixed

--- rpg-gamifying-life/src/main.jsx
+++ rpg-gamifying-life/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.jsx'
+import Favicon from 'react-favicon';
+import Logo from '../public/images/marca.svg'
 
 import './index.css'
 
@@ -8,6 +10,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
+        <Favicon url={Logo}/>
     <App />
     </QueryClientProvider>
 )
