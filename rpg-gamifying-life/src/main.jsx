d91cee9b9fa42f146f2a37677366209bd657cf5e
omarbramirez <omar.ramirez94@hotmail.es>
2024-04-30 09:43:42 -0600

    favicon path fixed

--- rpg-gamifying-life/src/main.jsx
+++ rpg-gamifying-life/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.jsx'
 import Favicon from 'react-favicon';
-import Logo from '../public/images/marca.svg'
+import Logo from './marca.svg'
 
 import './index.css'
 
